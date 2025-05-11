443ca034b62a4e0091c352d595c49fd786321cb7
Tai Nguyen <tantantai@gmail.com>
2025-05-11 16:54:43 +0700

    GateWay add custom error page

---
+++ gateway-service/src/main/java/com/example/gatewayservice/controllers/CustomErrorController.java
@@ -0,0 +1,21 @@
+package com.example.gatewayservice.controllers;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class CustomErrorController implements ErrorController {
+
+    @ExceptionHandler(Throwable.class)
+    public ResponseEntity<?> handleException(Throwable e) {
+        StringWriter errors = new StringWriter();
+        e.printStackTrace(new PrintWriter(errors));
+        return new ResponseEntity<>("{}", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
